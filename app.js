e7c8b526713ad9631c5e21b3f86c1309cd0b082d
Sachin Verma <83693262+iam-sachinverma@users.noreply.github.com>
2023-05-02 15:26:17 +0530

    Global Error Handler Middleware

--- app.js
+++ app.js
@@ -12,4 +12,23 @@ app.use(express.json());
 // ROUTES
 app.use("/api/products", productRouter);
 
+// Error Handler Middlerware
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
